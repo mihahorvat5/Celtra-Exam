53c75dc47e2895c33117af5a833e3a01b310423d
mihahorvat5 <mihahorvat2002@gmail.com>
2025-07-23 11:29:19 +0200

    Initial commit with base project file structure

---
+++ src/views/HomeView.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="home-view">
+    <h1>July 2025</h1>
+    <p>Initial commit</p>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.home-view {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  text-align: center;
+  color: #6c757d;
+}
+
+h1 {
+  font-weight: 300;
+  font-size: 3rem;
+  margin-bottom: 1rem;
+}
+</style>
